6680904928865a2f73c015359ff95e347b8f6158
nrius <nuria.rius.camps@gmail.com>
2021-03-03 10:51:01 +0100

    adding changes to clean data

--- Code/cleaning_data.py
+++ Code/cleaning_data.py
@@ -3,6 +3,10 @@
 import pandas as pd
 import numpy as np
 
+#nurias version: do not drop density, keep values under 2.
+#remove weight under 0
+#log transform sugar to flour
+
 def dropping_rows_and_columns(df):
     '''
     dropping diameter becuase it has no sdt dev, all values are the same
@@ -13,11 +17,21 @@ def dropping_rows_and_columns(df):
     df.dropna(how='any', subset=['mixins'], inplace=True)
     df.drop(columns="chill time", inplace=True)
     df.drop(columns=["aesthetic appeal", "crunch factor", "density"], inplace=True)
-    df.drop(df[(df["calories"] < 0) | (df["calories"] > 300)].index, inplace=True)
+    df.drop(df[df["calories"] < 0].index, inplace=True) 
+    df.drop(df[df["weight"] < 0].index, inplace=True)
     df.drop(df[df["grams baking soda"] > 1.2].index, inplace=True)
+    df.drop(df[df["sugar to flour ratio"] > 1].index, inplace=True)
+    
     
     # converting sugar index to numpy
     sugar_index = np.array(df["sugar index"])
     df["sugar index"] = np.log(sugar_index)
+    
+    
+    # converting bake temp ratio to numpy
+    #prepare columns to apply log
+    
+    #bake_temp = np.array(df["bake temp"])
+    #df["bake temp"] = np.log(bake_temp)
     
     return df
